3559e68a85d946034a93ebf74affc2b5656fd3fa
thanhtd <hinhtudien@gmail.com>
2023-12-26 21:30:58 +0700

    Final done

--- diff_integral/TaylorDiff.m
+++
@@ -1,4 +0,0 @@
-function result = TaylorDiff(xa, ya, v, h)
-    i = find(xa == v);
-    result = (ya(i + 1) - ya(i)) / h;
-end
--- differential_equation/ole.m
+++
@@ -1,9 +0,0 @@
-function [x, y] = ole(fxy, x0, xn, y0, N)
-    h = (xn - x0) / N;
-    x(1) = x0; y(1) = y0;
-
-    for i = 2 : N
-        x(i) = i * 0.2;
-        y(i) = y(i - 1) + h * fxy(x(i - 1), y(i - 1));
-    end
-end
